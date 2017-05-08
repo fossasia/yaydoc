b46015b61613961c7b9aa5a21f6b5f2edced1651
root <rbalajis25@gmail.com>
2017-05-08 17:17:30 +0530

    token added

--- generate.sh
+++ generate.sh
@@ -10,7 +10,7 @@ cd temp
 make html
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --quiet --branch=gh-pages git@github.com:sch00lb0y/tempCheck.git gh-pages
+git clone --quiet --branch=gh-pages https://a4839d23df317c6fb787216d8095ef8a3c9ef15f@github.com/sch00lb0y/tempCheck.git gh-pages
 cd gh-pages
 git rm -rf ./*
 cp -a ../_build/html/. ./
