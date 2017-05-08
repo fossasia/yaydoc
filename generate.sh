9cafa2f07741f57638023235e0d812fdb09b0eab
root <rbalajis25@gmail.com>
2017-05-08 17:26:56 +0530

    github token changed

--- generate.sh
+++ generate.sh
@@ -8,7 +8,7 @@ cd ..
 cp -a doc/. temp/
 cd temp
 make html
-git clone --quiet --branch=gh-pages https://a4839d23df317c6fb787216d8095ef8a3c9ef15f@github.com/sch00lb0y/tempCheck.git gh-pages
+git clone --quiet --branch=gh-pages https://0afd38adfa0ef6f74e828a9b130ef660c8f4d83f:x-oauth-basic@github.com/sch00lb0y/tempCheck.git gh-pages
 cd gh-pages
 git rm -rf ./*
 cp -a ../_build/html/. ./
