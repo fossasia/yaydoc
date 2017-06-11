6b2d2eaff34cacbb3c4ba1504197a622077eddda
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2017-06-11 19:19:58 +0530

    Fixes #100 Implement Download as Zip functionality

--- WebUI/routes/index.js
+++ WebUI/routes/index.js
@@ -1,8 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var util = require("util");
-var exec = require("child_process").exec;
-
+var spawn = require('child_process').spawn;
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -10,6 +9,7 @@ router.get("/", function(req, res, next) {
 });
 
 router.post("/generate", function (req, res, next) {
+  var email = req.body.email;
   var author = req.body.author;
   var gitUrl = req.body.git_url;
   var docTheme = req.body.doc_theme;
@@ -17,14 +17,39 @@ router.post("/generate", function (req, res, next) {
   var projectName = req.body.project_name;
   var version = req.body.version;
 
-  exec("./generate.sh -g " + gitUrl + " -a " + author + " -t " + docTheme + " -p " + docPath + " -o " + projectName
-      + " -v " + version, function (error, stdout, stderr) {
-      //
-  }).on("exit", function (code) {
-      if (code === 0) {
-        res.render("success");
-      }
+  const args = [
+    "-g", gitUrl,
+    "-a", author,
+    "-t", docTheme,
+    "-p", docPath,
+    "-o", projectName,
+    "-v", version,
+    "-m", email
+  ];
+  
+  var process = spawn("./generate.sh", args);
+  
+  process.stdout.on('data', function (data) {
+    console.log(data.toString());
   });
+  process.stderr.on('data', function (data) {
+    console.log(data.toString());
+  });
+  
+  process.on('exit', function (code) {
+    console.log('Generation script exited with code ' + code);
+    if (code === 0) {
+      res.render('success', {
+        email: email,
+        projectName: projectName
+      });
+    }
+  });
+});
+
+router.get('/download/:email/:projectName', function (req, res, next) {
+  var file = __dirname + '/../temp/' + req.params.email + '/' + req.params.projectName + '.zip';
+  res.download(file);
 });
 
 module.exports = router;
