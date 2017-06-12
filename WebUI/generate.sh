5f2e55512b7b3f3a87f7116e7297caa238399e62
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2017-06-13 02:05:22 +0530

    Fixes #110 Enable pipelining of Bash Script output to UI

--- WebUI/generate.sh
+++ WebUI/generate.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "Setting Environment Variables..."
-while getopts g:a:t:p:o:v:m: option
+while getopts g:a:t:p:o:v:m:u: option
 do
  case "${option}"
  in
@@ -12,21 +11,23 @@ do
  o) PROJECTNAME=${OPTARG};;
  v) VERSION=${OPTARG};;
  m) EMAIL=${OPTARG};;
+ u) UNIQUEID=${OPTARG};;
  esac
 done
-echo "Done..."
 
 mkdir -p temp/${EMAIL} && cd $_
-echo "Moved to $(pwd)"
-echo "Cloning Repository..."
+
+echo -e "Cloning Repository...\n"
 git clone -q ${GITURL} "$PROJECTNAME" && cd $_
-echo "Repository Cloned"
+echo -e "Repository Cloned Successfully!\n"
+
 REPO=$(git config remote.origin.url)
 
 # Create an isolated Python environment
+echo -e "Creating an isolated Python environment\n"
 virtualenv -q --python=python $HOME/yaydocvenv
 source $HOME/yaydocvenv/bin/activate
-
+echo -e "Python environment created successfully!\n"
 
 ROOT_DIR=$(pwd)
 
@@ -34,22 +35,22 @@ ROOT_DIR=$(pwd)
 if [ "$DOCPATH" != "." ]; then
   cd $DOCPATH/../
 fi
-echo $DOCPATH
-pwd
 mkdir yaydoctemp
 BUILD_DIR=$(pwd)/yaydoctemp
 
 cd $ROOT_DIR
 
-cp -a ../../../scripts/ $BUILD_DIR/
-cp -a ../../../templates/ $BUILD_DIR/
-cp -a ../../../requirements.txt $BUILD_DIR/
+cp -a ../../../../scripts/ $BUILD_DIR/
+cp -a ../../../../templates/ $BUILD_DIR/
+cp -a ../../../../requirements.txt $BUILD_DIR/
 
 cd ${BUILD_DIR}
 mkdir _themes
 
 # Install packages required for documentation generation
+echo -e "Installing packages required for documentation generation\n"
 pip install -q -r requirements.txt
+echo -e "Generation successful\n"
 
 URL_SPLIT=(${REPO//// })
 AUTHOR=${AUTHOR:-${URL_SPLIT[2]}}
@@ -57,16 +58,18 @@ DEFAULT_NAME=(${URL_SPLIT[3]//./ })
 PROJECTNAME=${PROJECTNAME:-${DEFAULT_NAME}}
 
 # Setting up documentation sources
+echo -e "Setting up documentation sources\n"
 sphinx-quickstart --ext-githubpages -q -v "${VERSION:-development}" -a "$AUTHOR" -p "$PROJECTNAME" -t templates/ -d html_theme=${DOCTHEME:-alabaster} -d html_logo=${LOGO:-} > /dev/null
 if [ $? -ne 0 ]; then
   echo -e "Failed to initialize build process.\n"
   exit 1
 fi
+echo -e "Documentation setup successful!\n"
 
 rm index.rst
 cd $ROOT_DIR
 
-cp -a ../../../fossasia $BUILD_DIR/_themes/
+cp -a ../../../../fossasia $BUILD_DIR/_themes/
 
 # Extract markup files from source repository and extend pre-existing conf.py
 if [ -f $DOCPATH/conf.py ]; then
@@ -77,15 +80,20 @@ fi
 rsync -a --exclude=conf.py --exclude=yaydoctemp $DOCPATH/ $BUILD_DIR/
 cd $BUILD_DIR
 
+echo -e "Starting Documentation Generation...\n"
 make html
 if [ $? -ne 0 ]; then
   echo -e "Failed to generate documentation.\n"
   exit 2
 fi
+echo -e "Documentation Generated Successfully!\n"
 
-mv $BUILD_DIR/_build/html $ROOT_DIR/../${PROJECTNAME}_preview && cd $_/../
-zip -r -q ${PROJECTNAME}.zip ${PROJECTNAME}_preview
-if [ $? -eq 0 ]; then
-  echo -e "Generated docs.\n"
-  exit 0
+echo -e "Setting up documentation for Download and Preview\n"
+mv $BUILD_DIR/_build/html $ROOT_DIR/../${UNIQUEID}_preview && cd $_/../
+zip -r -q ${UNIQUEID}.zip ${UNIQUEID}_preview
+if [ $? -ne 0 ]; then
+  echo -e "Failed setting up.\n"
+  exit 3
 fi
+
+echo -e "Documentation setup successful!\n"
--- WebUI/routes/index.js
+++ WebUI/routes/index.js
@@ -1,54 +1,13 @@
 var express = require("express");
 var router = express.Router();
-var util = require("util");
-var spawn = require('child_process').spawn;
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Yaydoc" });
 });
 
-router.post("/generate", function (req, res, next) {
-  var email = req.body.email;
-  var author = req.body.author;
-  var gitUrl = req.body.git_url;
-  var docTheme = req.body.doc_theme;
-  var docPath = req.body.doc_path;
-  var projectName = req.body.project_name;
-  var version = req.body.version;
-
-  const args = [
-    "-g", gitUrl,
-    "-a", author,
-    "-t", docTheme,
-    "-p", docPath,
-    "-o", projectName,
-    "-v", version,
-    "-m", email
-  ];
-  
-  var process = spawn("./generate.sh", args);
-  
-  process.stdout.on('data', function (data) {
-    console.log(data.toString());
-  });
-  process.stderr.on('data', function (data) {
-    console.log(data.toString());
-  });
-  
-  process.on('exit', function (code) {
-    console.log('Generation script exited with code ' + code);
-    if (code === 0) {
-      res.render('success', {
-        email: email,
-        projectName: projectName
-      });
-    }
-  });
-});
-
-router.get('/download/:email/:projectName', function (req, res, next) {
-  var file = __dirname + '/../temp/' + req.params.email + '/' + req.params.projectName + '.zip';
+router.get('/download/:email/:uniqueId', function (req, res, next) {
+  var file = __dirname + '/../temp/' + req.params.email + '/' + req.params.uniqueId + '.zip';
   res.download(file);
 });
 
