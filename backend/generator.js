a1da62a760855bad02768ee2754261f506e9707d
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2017-06-16 21:21:38 +0530

    Fixes #128 Handle form input validation (#131)

--- backend/generator.js
+++ backend/generator.js
@@ -1,8 +1,13 @@
 var exports = module.exports = {};
 
 var uuidV4 = require("uuid/v4");
+var validation = require("../public/scripts/validation.js");
 
 exports.executeScript = function (socket, formData) {
+  if (!validation.isValid(formData)) {
+    socket.emit('err-logs', "Failed to generated documentation due to an error in input fields");
+    return false;
+  }
   var spawn = require('child_process').spawn;
 
   var email = formData.email;
@@ -37,6 +42,9 @@ exports.executeScript = function (socket, formData) {
     console.log('child process exited with code ' + code);
     if (code === 0) {
       socket.emit('success', {email: email, uniqueId: uniqueId});
+    } else {
+      socket.emit('failure', {errorCode: code});
     }
   });
+  return true;
 };
