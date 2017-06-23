b00153f3d8102489f958ff323ef95063d4c3bdd0
Priyam Singh <priyamsingh.22296@gmail.com>
2017-06-23 22:41:31 +0530

    Fixes #163 add tests for exit codes by generate.sh

--- tests/generate_test.sh
+++ tests/generate_test.sh
@@ -1,7 +1,50 @@
 #!/bin/bash
 
-testEquality() {
-	assertEquals 1 1
+
+clean() {
+  rm -rf yaydoctemp
+  rm -rf yaydocclone
+  rm -rf temp
 }
+
+
+setUp() {
+  alias exit=return
+}
+
+
+tearDown() {
+  unset exit
+  clean
+}
+
+
+testErrorOnSphinxQuickstart() (
+  sphinx-quickstart() { return 1; }
+  source ./generate.sh > /dev/null 2>&1
+  assertEquals 1 $?
+)
+
+
+testErrorOnMake() (
+  make() { return 1; }
+  source ./generate.sh > /dev/null 2>&1
+  assertEquals 2 $?
+)
+
+
+testErrorOnZip() (
+  zip() { return 1; }
+  # shellcheck disable=SC2034
+  {
+  WEBUI="true"
+  GITURL="https://github.com/fossasia/yaydoc.git"
+  EMAIL="fossasia@gmail.com"
+  UNIQUEID="qwertyuiopasdfghjklzxcvbnm"
+  }
+  source ./generate.sh > /dev/null 2>&1
+  assertEquals 3 $?
+)
+
 
 . shunit2-2.1.6/src/shunit2
