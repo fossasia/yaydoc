04d9966c86e75918d42a2cbc342de5a46fb85f3a
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2017-07-28 20:21:39 +0530

    Fixes #302 Add status badge for buildstatus of repos

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var passport = require("passport");
 var crypter = require("../util/crypter.js");
 var validation = require("../public/scripts/validation.js");
 
+Repository = require("../model/repository");
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
   var isLoggedIn = false;
@@ -27,6 +29,39 @@ router.get('/download/:email/:uniqueId', function (req, res, next) {
 router.get('/logs/:type/:email/:uniqueId', function (req, res, next) {
   var file = __dirname + '/../temp/' + req.params.email + '/' + req.params.type + '_' + req.params.uniqueId + '.txt';
   res.download(file);
+});
+
+/* Generate SVG image for build status of repository */
+router.get('/:owner/:reponame.svg', function (req, res, next) {
+  Repository.getBuildStatusByRepositoryName(req.params.owner + '/' + req.params.reponame, function (error, result) {
+    var buildStatus = 'invalid';
+    var width = '94';
+    var color = '#9f9f9f';
+    var x = '70.5';
+    if (result === null || error) {
+      res.status(404);
+    } else {
+      if (result.buildStatus) {
+        buildStatus = 'success';
+        width = '102';
+        color = '#97CA00';
+        x = '74.5';
+      } else {
+        buildStatus = 'failed';
+        width = '88';
+        color = '#E05d44';
+        x = '67.5'
+      }
+    }
+
+    res.set('Content-Type', 'image/svg+xml');
+    res.render("badge", {
+      status: buildStatus,
+      width: width,
+      color: color,
+      x: x,
+    });
+  });
 });
 
 module.exports = router;
